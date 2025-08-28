ee22541dd3e630d4c16f9f5ab6b137f56de9f594
Ali Ramazon <mr.safarali@gmail.com>
2025-08-27 19:42:55 -0600

    Move target to the end of array: V2

--- 04_move-target-to-the-end.py
+++ 04_move-target-to-the-end.py
@@ -10,7 +10,7 @@ Examples:
 """
 
 
-def move_target_to_the_end(numbers, target):
+def move_target_to_the_end_v1(numbers, target):
     slow = 0
     for idx, value in enumerate(numbers):
         if value != target:
@@ -19,11 +19,27 @@ def move_target_to_the_end(numbers, target):
     return numbers
 
 
+def move_target_to_the_end_v2(numbers, target):
+    insert_pos = 0
+
+    for value in numbers:
+        if value != target:
+            numbers[insert_pos] = value
+            insert_pos += 1
+
+    while insert_pos < len(numbers):
+        numbers[insert_pos] = target
+        insert_pos += 1
+    return numbers
+
+
 print(
-    move_target_to_the_end([1, 8, 4, 11, 67, 8, 900, 12, 8, 13], 8)
+    move_target_to_the_end_v2([1, 8, 4, 11, 67, 8, 900, 12, 8, 13], 8)
     == [1, 4, 11, 67, 900, 12, 13, 8, 8, 8]
 )
-print(move_target_to_the_end([5, 3, 5, 2, 1, 5, 7, 9], 5) == [3, 2, 1, 7, 9, 5, 5, 5])
-print(move_target_to_the_end([], 1) == [])
-print(move_target_to_the_end([1, 1, 1, 1], 1) == [1, 1, 1, 1])
-print(move_target_to_the_end([10, 20, 30], 5) == [10, 20, 30])
+print(
+    move_target_to_the_end_v2([5, 3, 5, 2, 1, 5, 7, 9], 5) == [3, 2, 1, 7, 9, 5, 5, 5]
+)
+print(move_target_to_the_end_v2([], 1) == [])
+print(move_target_to_the_end_v2([1, 1, 1, 1], 1) == [1, 1, 1, 1])
+print(move_target_to_the_end_v2([10, 20, 30], 5) == [10, 20, 30])
